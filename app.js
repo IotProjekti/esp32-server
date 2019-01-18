8a72177eafecee797c2fb1f09a5c22e38f90d5d3
Mikko Maja <mikmaj88@gmail.com>
2019-01-18 13:47:47 +0200

    Add mock data

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const port = process.env.PORT || 3000
+const request = require('request');
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send(JSON.stringify({ Hello: 'World' }))
-})
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+request.get(url, (error, response, body) => {
+    let json = JSON.parse(body);
+    console.log(body);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
