1cdb6b90e5ab37ca3505e29a0da4f4cfdb7573e1
Mikko Maja <mikmaj88@gmail.com>
2019-01-25 11:20:03 +0200

    Add POST route

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/data', (req, res) => {
     res.send(data)
 })
 
+app.post('/postdata', (req, res) => {
+	let data = req.body.data; // your data
+    // do something with that data (write to a DB, for instance)
+	res.status(200).json({
+		message: "Data received successfully"
+	});
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
