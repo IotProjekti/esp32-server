bca01fa6172e4cd9b77ae1126a532f2b5093a32b
Mikko Maja <mikmaj88@gmail.com>
2019-02-06 12:56:03 +0200

    Add handlebars templating

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const hbs = require('hbs')
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.json())
 
-let sensorData = []
+
+let sensorData = [100, 200, 300]
+
+
+app.get('/', (req, res) => {
+    res.render('trafficlight.hbs', {
+        value: sensorData[0]
+    })
+})
 
 app.get('/data', (req, res) => {
     res.send(sensorData)
