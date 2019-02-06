932745ec58f9af6bc5d2c03ffd4f26d4f7c7f3be
Mikko Maja <mikmaj88@gmail.com>
2019-02-06 14:07:51 +0200

    Add traffic light functionality

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
 const hbs = require('hbs')
+const path = require('path')
 const port = process.env.PORT || 3000
 
 const app = express()
 
 app.set('view engine', 'hbs')
 app.use(express.json())
+app.use(express.static(path.join(__dirname + '/public')))
 
-
-let sensorData = [100, 200, 300]
-
+let sensorData = [100, 200, 300, 20]
+let red = false
+let yellow = false
+let green = false
 
 app.get('/', (req, res) => {
-    res.render('trafficlight.hbs', {
-        value: sensorData[0]
-    })
+    if (sensorData[sensorData.length - 1] < 100) {
+        res.render('trafficlight.hbs', {
+            green: true
+        })
+    } else if (sensorData[sensorData.length - 1] >= 100 && sensorData[sensorData.length - 1] < 200) {
+        res.render('trafficlight.hbs', {
+            yellow: true
+        })
+    } else {
+        res.render('trafficlight.hbs', {
+            red: true
+        })
+    }
 })
 
 app.get('/data', (req, res) => {
